4563a22e2f1b91d1ea504ff2181ee661fb665c79
wilmerpe <wilmer.wmpac@gmail.com>
2023-01-27 19:52:16 +0100

    2commit

---
+++ ReactAvanzado/AppListaDeTareas/proyecto/src/components/todo.js
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+export default function Todo({ item, onUpdate, onDelete }) {
+	const [isEdit, setIsEdit] = useState(false);
+
+	function FormEdit() {
+
+		const [newValue, setNewValue] = useState(item.title);
+
+		function handleSubmit(e) {
+			e.preventDefault();
+		}
+
+		function handleChange(e) {
+			const value = e.target.value;
+			setNewValue(value);
+		}
+
+		function handleClick() {
+			onUpdate(item.id, newValue);
+			setIsEdit(false);
+		}
+		return <form className="todoUpdateForm" onSubmit={handleSubmit}>
+			<input type="text" className="todoInput" onChange={handleChange} value={newValue}></input>
+			<button className="button" onClick={handleClick}>Update</button>
+		</form>
+	}
+
+	function TodoElement() {
+		return <div className="todoInfo">
+			<span className="todoTitle">{item.title} </span>
+			<button className="button" onClick={() => setIsEdit(true)}>Edit</button>
+			<button className="buttonDelete" onClick={() => onDelete(item.id)}>Delete</button>
+		</div>
+	}
+	return (
+		<div className="todo">
+			{isEdit ? <FormEdit /> : <TodoElement />}
+		</div>
+	)
+}
